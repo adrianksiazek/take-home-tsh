27da4fbf2c08839ffe7bd31d31aa330332840c14
Adrian Książek <adrian.ksiazek@sagiton.pl>
2023-11-15 08:38:29 +0100

    refactor: lifting up code

--- src/app/default/home/components/products/Products.tsx
+++ src/app/default/home/components/products/Products.tsx
@@ -1,50 +1,40 @@
 import { Loader } from 'ui';
 
 import { useProducts } from './Products.effect';
-import { Product } from './product/Product';
 import { ProductsEmptyList } from './ProductEmptylist';
 import { Pagination } from './products-pagination/ProductsPagination';
+import { ProductList } from './product-list/ProductList';
 
 export const Products = () => {
-  const { productList, isLoading, isError, setPage, page, lastPage } = useProducts();
+  const { productList, isLoading, isError, setPage, page } = useProducts();
 
-  return (
-    <>
-      {isLoading ? (
-        <Loader />
-      ) : isError ? (
-        <div className="absolute left-1/2 top-1/2 -translate-y-1/2 bg-white p-12">
-          <h2>An error occured</h2>
-        </div>
-      ) : (
-        <>
-          {productList && productList.items.length > 0 && (
-            <div className="grid place-content-center gap-4 py-12 2xs:grid-cols-2 lg:grid-cols-4">
-              {productList?.items.map((product, index) => (
-                <Product
-                  key={index}
-                  name={product.name}
-                  description={product.description}
-                  rating={product.rating}
-                  active={product.active}
-                  image={product.image}
-                  promo={product.promo}
-                  buttonText="Show details"
-                  data-testid="pw-product"
-                />
-              ))}
-            </div>
-          )}
-          {productList && productList.items.length === 0 && <ProductsEmptyList />}
-          {productList && productList.items.length > 0 && (
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (isError) {
+    return (
+      <div className="absolute left-1/2 top-1/2 -translate-y-1/2 bg-white p-12">
+        <h2>An error occured</h2>
+      </div>
+    );
+  }
+
+  if (productList) {
+    return (
+      <>
+        {productList.items.length > 0 && (
+          <>
+            <ProductList productList={productList.items} />
             <Pagination
               currentPage={page ? Number(page) : productList.meta.currentPage}
-              totalPages={lastPage}
+              totalPages={productList.meta.totalPages}
               setPage={setPage}
             />
-          )}
-        </>
-      )}
-    </>
-  );
+          </>
+        )}
+        {productList.items.length === 0 && <ProductsEmptyList />}
+      </>
+    );
+  }
 };
---
+++ src/app/default/home/components/products/product-list/ProductList.tsx
@@ -0,0 +1,24 @@
+import { Product } from '../product/Product';
+import { Product as ProductModel } from 'api/actions/product/product.types';
+
+type ProductListProps = {
+  productList: ProductModel[];
+};
+
+export const ProductList = ({ productList }: ProductListProps) => (
+  <div className="grid place-content-center gap-6 py-12 2xs:grid-cols-2 lg:grid-cols-4">
+    {productList.map((product, index) => (
+      <Product
+        key={index}
+        name={product.name}
+        description={product.description}
+        rating={product.rating}
+        active={product.active}
+        image={product.image}
+        promo={product.promo}
+        buttonText="Show details"
+        data-testid="pw-product"
+      />
+    ))}
+  </div>
+);
